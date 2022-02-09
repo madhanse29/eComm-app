ca896509924f3e8e98d3d5444ae78cd98ea3b00c
madhanse29 <srkmadhan@gmail.com>
2022-02-09 17:13:38 +0530

    header login footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,10 @@
+<div class="card">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-footer">
+    <blockquote class="blockquote mb-0">
+   
+    </blockquote>
+  </div>
+</div>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,27 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link ">Cart(0)</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success text-light" type="submit">Search</button>
+      </form>
+    </div>
+ 
+</nav>
+</div>
